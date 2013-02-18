82d48df3483b4a7804507480c62a1500804ff164
Stuart Wakefield <stuie@live.co.uk>
2013-02-18 16:22:54 +0000

    Tests are old using old version of Router

--- test/router_test.js
+++ test/router_test.js
@@ -2,55 +2,19 @@ tester.addTest("Router", {
 	
 	setUp: function() {
 		this.dispatcher = new Dispatcher();
+		this.router = new Router(this.dispatcher);
 		this.handler = {
 			handle: function(args) {
 				this.called = true;
 			}
 		};
 	},
-	
-	testBasicRoute: function() {
-		var router = this.createRouter([
-			"test/route", "test.route"
-		]);
-		assertion.isTrue(router.find("test/route") != -1);
-	},
-	
-	testVariableRoute: function() {
-		var router = this.createRouter([
-			"test/route/@id", "test.route"
-		]);
-		assertion.isTrue(router.find("test/route/something") != -1);
-	},
-	
-	testRouteNormalization: function() {
-		var router = this.createRouter([
-			"#/test/route/@id/", "test.route"
-		]);
-		assertion.isTrue(router.find("test/route/something") != -1);
-		assertion.isTrue(router.find("#/test/route/something/") != -1);
-	},
-	
-	testRouteResolution: function() {
-		var router = this.createRouter([
-			"#/test/route/@id/", "test.route"
-		]);
-		ev = router.resolve("#/test/route/something/");
-		assertion.areEqual("test.route", ev.name);
-		assertion.areEqual("something", ev.args.id);
-	},
-	
+		
 	testRoute: function() {
-		var router = this.createRouter([
-			"test/route", "test"
-		]);
+		this.router.bind("test/route", "test");
 		this.dispatcher.bind("test", this.handler);
-		router.route("#/test/route/");
+		this.router.route("#/test/route/");
 		assertion.isTrue(this.handler.called);
-	},
-	
-	createRouter: function(routes) {
-		return new Router(this.dispatcher, routes);
 	}
 	
 });
